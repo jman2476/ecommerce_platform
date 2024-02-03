0199e4ac0ed0a7a9fbe86bdc4bc6506868a10d87
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-02-03 03:51:35 -0500

    finished the routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',  async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [
+        {model: Category},
+        {model: Tag}
+      ]
+    })
+
+    res.json(products)
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const product_id =  req.params.id
+  try {
+    const products = await Product.findByPk(product_id,{
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag
+        }
+      ]
+    })
+
+    res.json(products)
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 // create new product
@@ -92,8 +121,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product_id = req.params.id
+    const prodToDelete = await Product.findByPk(product_id)
+
+    await prodToDelete.destroy()
+
+    res.json({
+      message: 'Product annihilated'
+    })
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,85 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: Product
+    })
+
+    res.json(tags)
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag_id = req.params.id
+    const tags = await Tag.findByPk(tag_id, {
+      include: Product
+    })
+
+    res.json(tags)
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = req.body
+    const tag = await Tag.create(newTag)
+
+    res.json({
+      message: 'New tag created',
+      tag
+    })
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag_id = req.params.id
+    const newTagName= req.body.tag_name
+    const tagToUpdate = await Tag.findByPk(tag_id)
+
+    tagToUpdate.tag_name = newTagName
+
+    await tagToUpdate.save()
+
+    res.json({
+      message: 'Tag updated successfully',
+      tagToUpdate
+    })
+
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag_id = req.params.id
+    const tagToDelete = await Tag.findByPk(tag_id)
+
+    await tagToDelete.destroy()
+
+    res.json({
+      message: 'Tag annihilated'
+    })
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 module.exports = router;
